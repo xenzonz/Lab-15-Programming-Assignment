3bd94ac3a7f90fed975db2f40c0f1285029c00ee
sam cocquyt <samcocquyt04@gmail.com>
2026-05-03 22:17:28 -0400

    main spiral plot code added and image generated

--- Lab15_xenzonz-1.py
+++ Lab15_xenzonz-1.py
@@ -12,9 +12,33 @@ v. 5/3/2026
 import math
 import matplotlib.pyplot as plt
 
-def main():
-    pass
 
+def spiral_data():
+    x_values = []
+    y_values = []
+
+    for degrees in range(0, 360):
+        radians = math.radians(degrees)
+        radius = degrees / 360
+
+        x = radius * math.cos(radians)
+        y = radius * math.sin(radians)
+
+        x_values.append(x)
+        y_values.append(y)
+    
+    return x_values, y_values
+
+        
+
+def create_spiral(x_values, y_values):
+    plt.plot(x_values, y_values)
+    plt.savefig("spiralplot.png")
+    plt.show
+
+def main():
+    x_values, y_values = spiral_data()
+    create_spiral(x_values, y_values)
 
 if __name__ == "__main__":
     main()
