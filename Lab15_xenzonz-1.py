f3434ebead4517485661d023bdaf663f152fe5ae
sam cocquyt <samcocquyt04@gmail.com>
2026-05-03 22:09:04 -0400

    imported math and matplotlib and added main functions

--- Lab15_xenzonz-1.py
+++ Lab15_xenzonz-1.py
@@ -1,1 +1,20 @@
 print("hello world")
+
+"""
+Docstring for Lab15_xenzonz-1.py
+i. Lab 15: Plot a Math Formula
+ii. Sam Cocquyt
+iii. A program that uses matplotlib to plot a spiral
+iv. No starter code
+v. 5/3/2026
+"""
+
+import math
+import matplotlib.pyplot as plt
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
